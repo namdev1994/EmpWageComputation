d7352b08f9a7279f76d0de9fe964f7f316fbede6
namdev1994 <n.lavhare1994@gmail.com>
2021-03-29 23:45:53 +0530

    Added: EmpWage Calculation Using While Condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,27 +1,26 @@
+isFullTime=1;
+isPartTime=2;
 empRatePerHr=20
-isFullTime=1
-isPartTime=2
 numOfWorkingDays=20
-totalSalary=0
-for (( i=1; i<$numOfWorkingDays; i++ ))
+maxHrInMonth=100
+totalEmpHrs=0
+totalWorkingDays=0
+while [[ $totalEmphrs -lt $maxHrInMonth && $totalWorkingDays -lt $numOfWorkingDays ]]
 do
+		((totalWorkingDays++))
 
 		randomCheck=$((RANDOM%3))
 
-if [ $randomCheck -eq $isFullTime ]
-then
+case $randomCheck in
 
-		empHr=8
-		salary=$(($empRatePerHr*$empHr))
-		totalSalary=$(($totalSalary+$salary))
-elif [ $randomCheck -eq $isPartTime ]
-then
-
-		empHr=4
-		salary=$(($empRatePerHr*$empHr))
-		totalSalary=$(($totalSalary+$salary))
-else
-		empHr=0
-fi
+				$isFullTime)
+							empHr=8 ;;
+				$isPartTime)
+							empHr=4 ;;
+				*)
+							empHr=0 ;;
+esac
+totalEmpHrs=$(($totalEmpHrs+$empHr))
 done
+totalSalary=$(($totalEmpHrs*$empRatePerHr))
 echo $totalSalary
